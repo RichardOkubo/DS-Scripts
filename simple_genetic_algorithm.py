97d42cfd7a9ad5223b5d643c15d7a8e8f50f564c
Richard Okubo <richardlopesneves@gmail.com>
2021-04-19 00:29:06 -0300

    Up

---
+++ simple_genetic_algorithm.py
@@ -0,0 +1,160 @@
+from random import random
+
+
+class Individuo:
+    def __init__(self, espacos, valores, limite_espacos, geracao=0):
+        self.espacos = espacos
+        self.valores = valores
+        self.limite_espacos = limite_espacos
+        self.nota_avaliacao = 0
+        self.espaco_usado = 0
+        self.geracao = geracao
+        self.cromossomo = []
+
+        for i in range(len(espacos)):
+            if random() < 0.5:
+                self.cromossomo.append("0")
+            else:
+                self.cromossomo.append("1")
+
+    def avaliacao(self):
+        nota = 0
+        soma_espacos = 0
+        for i in range(len(self.cromossomo)):
+            if self.cromossomo[i] == "1":
+                nota += self.valores[i]
+                soma_espacos += self.espacos[i]
+        if soma_espacos > self.limite_espacos:
+            nota = 1
+        self.nota_avaliacao = nota
+        self.espaco_usado = soma_espacos
+
+    def crossover(self, outro_individuo):
+        corte = round(random() * len(self.cromossomo))
+
+        filho1 = outro_individuo.cromossomo[0:corte] + self.cromossomo[corte::]
+        filho2 = self.cromossomo[0:corte] + outro_individuo.cromossomo[corte::]
+
+        filhos = [
+            Individuo(
+                self.espacos, self.valores, self.limite_espacos, self.geracao + 1
+            ),
+            Individuo(
+                self.espacos, self.valores, self.limite_espacos, self.geracao + 1
+            ),
+        ]
+        filhos[0].cromossomo = filho1
+        filhos[1].cromossomo = filho2
+        return filhos
+
+    def mutacao(self, taxa_mutacao):
+        # print("Antes %s " % self.cromossomo)
+        for i in range(len(self.cromossomo)):
+            if random() < taxa_mutacao:
+                if self.cromossomo[i] == "1":
+                    self.cromossomo[i] = "0"
+                else:
+                    self.cromossomo[i] = "1"
+        # print("Depois %s " % self.cromossomo)
+        return self
+
+
+class AlgoritmoGenetico:
+    def __init__(self, tamanho_populacao, ver_evolucao=True):
+        self.tamanho_populacao = tamanho_populacao
+        self.populacao = []
+        self.geracao = 0
+        self.melhor_solucao = 0
+        self.lista_solucoes = []
+        self.ver_evolucao = ver_evolucao  # Visualizar a evolução
+
+    def inicializa_populacao(self, espacos, valores, limite_espacos):
+        for i in range(self.tamanho_populacao):
+            self.populacao.append(Individuo(espacos, valores, limite_espacos))
+        self.melhor_solucao = self.populacao[0]
+
+    def ordena_populacao(self):
+        self.populacao = sorted(
+            self.populacao, key=lambda populacao: populacao.nota_avaliacao, reverse=True
+        )
+
+    def melhor_individuo(self, individuo):
+        if individuo.nota_avaliacao > self.melhor_solucao.nota_avaliacao:
+            self.melhor_solucao = individuo
+
+    def soma_avaliacoes(self):
+        soma = 0
+        for individuo in self.populacao:
+            soma += individuo.nota_avaliacao
+        return soma
+
+    def seleciona_pai(self, soma_avaliacao):
+        pai = -1
+        valor_sorteado = random() * soma_avaliacao
+        soma = 0
+        i = 0
+        while i < len(self.populacao) and soma < valor_sorteado:
+            soma += self.populacao[i].nota_avaliacao
+            pai += 1
+            i += 1
+        return pai
+
+    def visualiza_geracao(self):
+        melhor = self.populacao[0]
+        print(
+            f"""
+G: {self.populacao[0].geracao,}
+Valor: {melhor.nota_avaliacao}
+Espaço: {melhor.espaco_usado}
+Cromossomo: {melhor.cromossomo}"""
+        )
+
+    def resolver(self, taxa_mutacao, numero_geracoes, espacos, valores, limite_espacos):
+        self.inicializa_populacao(espacos, valores, limite_espacos)
+
+        for individuo in self.populacao:
+            individuo.avaliacao()
+
+        self.ordena_populacao()
+        self.melhor_solucao = self.populacao[0]
+        self.lista_solucoes.append(self.melhor_solucao.nota_avaliacao)
+
+        if self.ver_evolucao:
+            self.visualiza_geracao()
+
+        for geracao in range(numero_geracoes):
+            soma_avaliacao = self.soma_avaliacoes()
+            nova_populacao = []
+
+            for individuos_gerados in range(0, self.tamanho_populacao, 2):
+                pai1 = self.seleciona_pai(soma_avaliacao)
+                pai2 = self.seleciona_pai(soma_avaliacao)
+
+                filhos = self.populacao[pai1].crossover(self.populacao[pai2])
+
+                nova_populacao.append(filhos[0].mutacao(taxa_mutacao))
+                nova_populacao.append(filhos[1].mutacao(taxa_mutacao))
+
+            self.populacao = list(nova_populacao)
+
+            for individuo in self.populacao:
+                individuo.avaliacao()
+
+            self.ordena_populacao()
+
+            if self.ver_evolucao:
+                self.visualiza_geracao()
+
+            melhor = self.populacao[0]
+            self.lista_solucoes.append(melhor.nota_avaliacao)
+            self.melhor_individuo(melhor)
+
+        print(
+            """\n---------------------- MELHOR SOLUÇÃO ----------------------
+- G: {self.melhor_solucao.geracao}
+- Valor: {self.melhor_solucao.nota_avaliacao}
+- Espaço: {self.melhor_solucao.espaco_usado}
+- Cromossomo: {self.melhor_solucao.cromossomo}"""
+        )
+
+        return self.melhor_solucao.cromossomo
